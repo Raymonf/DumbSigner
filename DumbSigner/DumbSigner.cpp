8532f9e440fe9e22ee9a48ca710850f5cffd71f2
Raymonf <raymonf@outlook.com>
2021-10-03 15:50:08 -0400

    Make things not Chinese

--- DumbSigner/DumbSigner.cpp
+++ DumbSigner/DumbSigner.cpp
@@ -1,6 +1,4 @@
-﻿// DumbSigner.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
-#include "shared.h"
+﻿#include "shared.h"
 #include "ProvisioningProfile.hpp"
 #include "Signer.hpp"
 #include <cxxopts.hpp>
